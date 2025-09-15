c4b6e043b21085d45d9017af892272af7f25300b
pranavg111 <pranavganeriwal111@gmail.com>
2025-09-15 16:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,21 @@ from langchain.chains import RetrievalQA
 loader = PyPDFLoader("Pranav_Resume.pdf")
 pages = loader.load()
 
-openai_api_key = "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"
-
 splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 docs = splitter.split_documents(pages)
 
 embedding_model = OpenAIEmbeddings(
     model="text-embedding-ada-002",
-    openai_api_key=openai_api_key
+    openai_api_key=os.getenv("OPENAI_API_KEY")
 )
+
 vectorstore = FAISS.from_documents(docs, embedding_model)
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
 
 llm = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0,
-    openai_api_key=openai_api_key
+    openai_api_key=os.getenv("OPENAI_API_KEY")
 )
 
 qa_chain = RetrievalQA.from_chain_type(
@@ -52,6 +51,7 @@ def ask_question():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
 
 
 
